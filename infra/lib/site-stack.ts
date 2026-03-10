57c74d39d7c15371cd286ccada6fd09a929fade4
Campbell Reid <campbell@campbellreid.com>
2026-03-11 00:27:06 +1000

    feat: Initialize Hugo site with PaperMod theme, new content, and CDK-based infrastructure deployment.

---
+++ infra/lib/site-stack.ts
@@ -0,0 +1,73 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export class HugoSiteStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 1. S3 Bucket for Static Content
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      bucketName: 'campbell-reid-content', // Should be unique or suffixed
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+    });
+
+    // 2. CloudFront Origin Access Control (OAC)
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'SiteOAC', {
+      originAccessControlConfig: {
+        name: 'HugoSiteOAC',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      },
+    });
+
+    // 3. CloudFront Distribution
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/404.html',
+        },
+      ],
+    });
+
+    // 4. Overwrite Origin Access Control for S3 (CDK L2 doesn't fully support OAC yet in some versions)
+    const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
+    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity', '');
+    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.0.OriginAccessControlId', oac.getAtt('Id'));
+
+    // 5. Bucket Policy to allow CloudFront Access
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+        },
+      },
+    }));
+
+    new cdk.CfnOutput(this, 'SiteDistributionId', {
+      value: distribution.distributionId,
+    });
+
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: siteBucket.bucketName,
+    });
+  }
+}
